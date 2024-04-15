ab7c9925f0ee7f0c126c82d0e6f4e02be34e452b
ZaidAhmad2 <165191865+ZaidAhmad2@users.noreply.github.com>
2024-04-15 16:46:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-FROM node:20.12.0
-# Set the working directory inside the container
-WORKDIR /usr/src/app
-
-# Copy both package.json and package-lock.json (if available)
-COPY package*.json ./
-
-# Install app dependencies
-RUN npm install
-
-# Copy the rest of the application code
-COPY . .
-
-# Command to run the app
-CMD ["node", "app.js"]
+console.log("HELLO ZABBIX I WILL TEST CPU AND MONITORING");
